fea1cca4c0988d216735dae1478129c25bb831c7
Nirmal Kumar S <nirmal@Nirmals-MacBook-Pro.local>
2019-09-03 00:01:47 +0530

    Added JS files

---
+++ js/conf/dev.js
@@ -0,0 +1,7 @@
+requirejs.config({
+    baseUrl: "..",
+    paths: {
+        'jquery': 'jquery-1.12.4',
+        'd3': 'd3.min'
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+require(['./conf/dev'], function(){
+    require(['jquery'], function($){
+        $(document).ready(function(){
+            console.log('Loaded');
+        });
+    });
+});
