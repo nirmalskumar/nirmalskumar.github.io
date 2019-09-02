f28f085820434ff9db68d55dd7f4768fa38d99ee
Nirmal Kumar S <nirmal@Nirmals-MacBook-Pro.local>
2019-09-03 00:43:32 +0530

    Added random number generator

--- js/data/mscombo_data.js
+++ js/data/mscombo_data.js
@@ -12,7 +12,19 @@ define(['d3'], function(d3){
     var from_date = '2019-08-01';
     var to_date = '2019-08-31';
 
-    var dt_format = d3.timeFormat("%m-%d-%Y");
+    var dt_format = d3.timeFormat("%Y-%m-%d");
+
+    var get_random_data = function(max){
+        var range = get_date_range();
+        range.forEach(function(date){
+            var value = Math.floor(Math.random() * Math.floor(max));
+            bar_dt['data'].push({
+                "label": date,
+                "value": value
+            });
+        });
+        return bar_dt;
+    };
     var get_date_range = function(){
         var date_range = d3.timeDays(new Date(2019, 7, 1), new Date(2019, 7, 31));
         var dt_range = date_range.map(function(d){
@@ -23,8 +35,8 @@ define(['d3'], function(d3){
 
 
     return {
-        getDateRange: function(){
-            return get_date_range();
+        getRandomData: function(){
+            return get_random_data(max);
         }
     }
 });
