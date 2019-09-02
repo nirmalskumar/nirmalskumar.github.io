29a073e8a2b80066b247a6682f069c6f6c4299d6
Nirmal Kumar S <nirmal@Nirmals-MacBook-Pro.local>
2019-09-03 00:44:50 +0530

    Added call to random data in main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ require(['./conf/dev'], function(){
     require(['jquery','data/mscombo_data'], function($, MSComboData){
         $(document).ready(function(){
             console.log('Loaded');
-            console.log(MSComboData.getDateRange());
+            console.log(MSComboData.getRandomData(20));
         });
     });
 });
