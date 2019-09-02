4a7ce3c1221e86871bea529fd2a38d8b77f67cdc
Nirmal Kumar S <nirmal@Nirmals-MacBook-Pro.local>
2019-09-03 00:36:31 +0530

    Modified date range calculation

--- js/data/mscombo_data.js
+++ js/data/mscombo_data.js
@@ -1,15 +1,24 @@
 define(['d3'], function(d3){
     var dt = [];
-    var bar_dt = {};
+    var bar_dt = {
+        "seriesName": "Usage",
+        "renderAs": "column",
+        "UOM": "kWh",
+        "data": []
+    };
     var line1_dt = {};
     var line2_dt = {};
 
     var from_date = '2019-08-01';
     var to_date = '2019-08-31';
 
+    var dt_format = d3.timeFormat("%m-%d-%Y");
     var get_date_range = function(){
         var date_range = d3.timeDays(new Date(2019, 7, 1), new Date(2019, 7, 31));
-        return date_range;
+        var dt_range = date_range.map(function(d){
+            return dt_format(d);
+        });
+        return dt_range;
     };
 
 
