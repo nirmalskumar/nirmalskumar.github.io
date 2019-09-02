5926c6ccdd3cdd79693db967ee240498071a852a
Nirmal Kumar S <nirmal@Nirmals-MacBook-Pro.local>
2019-09-03 01:34:47 +0530

    Added MSCombo initial version without styling

--- js/main.js
+++ js/main.js
@@ -2,7 +2,36 @@ require(['./conf/dev'], function () {
     require(['jquery', 'data/mscombo_data'], function ($, MSComboData) {
         $(document).ready(function () {
             console.log('Loaded');
-            console.log(MSComboData.getRandomData());
+            
+            var mscombo_data = MSComboData.getRandomData();
+            require(['chart/mscombo'], function(MS) {
+                MS.render({
+                    chart: {
+                        anchor: "#combo-chart",
+                        xAxis: {
+                            title: "Days",
+                            dateSeries: {
+                                format_date: true,
+                                input_dt_format: "YYYY-MM-DD",
+                                output_dt_format: "%d-%b",
+                                from_date: '2019-08-01',
+                                to_date: '2019-08-30'
+                            },
+                        },
+                        yAxis: {
+                            title: 'Usage',
+                            unit: 'kWh'
+                        },
+                        sYAxis: {
+                            title: "Temperature",
+                            unit: "F"
+                        },
+                        showValues: false,
+                        showGridLines: true,
+                    },
+                    dataset: mscombo_data
+                });
+            });
         });
     });
 });
